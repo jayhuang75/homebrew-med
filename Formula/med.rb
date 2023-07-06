0f83158c9135dc07317ec8deb1fcd30424c729af
wei <info@webdesignmtl.com>
2023-07-06 14:13:59 -0400

    downgrade version for github action testing

--- Formula/med.rb
+++ Formula/med.rb
@@ -1,10 +1,10 @@
 class Med < Formula
     desc "A simple enough CLI tool with auditable capability for Data Masking/Encyption/Decryption for CSV/JSON files."
     homepage "https://github.com/jayhuang75/homebrew-med"
-    url "https://github.com/jayhuang75/rust-cli-med/releases/download/0.6.2/macos_x86_archive-0.6.2.tar.gz"
-    sha256 "b35ee2ee615549f3b27741d8f1055ae94deb40b10601b678c6185542e48f0d01"
+    url "https://github.com/jayhuang75/rust-cli-med/releases/download/0.6.1/macos_x86_archive-0.6.1.tar.gz"
+    sha256 "13bb5e7155ed64f5bfa0640bc5412d2be97339173a31d89d9d1be06b0e19dbc5"
     license "Apache-2.0"
-    version "0.6.2"
+    version "0.6.1"
   
     def install
       bin.install "./med-0.6.2/med"
