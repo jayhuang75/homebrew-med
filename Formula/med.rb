5ba2a4f8ea00c1f7f8eb01c154626e7b1b3b1633
runner <runner@Mac-1688782471193.local>
2023-07-08 02:28:21 +0000

    med 0.6.4

--- Formula/med.rb
+++ Formula/med.rb
@@ -1,10 +1,10 @@
 class Med < Formula
     desc "A simple enough CLI tool with auditable capability for Data Masking/Encyption/Decryption for CSV/JSON files."
     homepage "https://github.com/jayhuang75/homebrew-med"
-    url "https://github.com/jayhuang75/rust-cli-med/releases/download/0.6.3/med-macos_x86_archive-0.6.3.tar.gz"
-    sha256 "e45313972db1aaef74546b87791e3f22c0d39ee789372eab2df41d69195998b3"
+    url "https://github.com/jayhuang75/rust-cli-med/releases/download/0.6.4/med-macos_x86_64-archive-0.6.4.tar.gz"
+    sha256 "abbb9d211d85c73a3d42e0f9bbde1cabaf3a96405ae887387fba4a5726f28d4b"
     license "Apache-2.0"
-    version "0.6.3"
+    version "0.6.4"
   
     def install
       bin.install "./med-0.6.3/med"
